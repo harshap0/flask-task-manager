ce21045efca5f5e6176df12f44ccaf2093d17ac7
Harsha patel <patelharsha371@gmail.com>
2026-02-20 23:19:09 +0530

    Fix SQLite absolute path

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
+import os
 
 app = Flask(__name__)
-
+BASE_DIR=os.path.dirname(os.path.abspath(__file__))
+DB_PATH=os.path.join(BASE_DIR,"tasks.db")
 
 def init_db():
-    conn = sqlite3.connect("tasks.db")
+    conn = sqlite3.connect("DB_PATH")
     cursor = conn.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tasks (
