de6ecf6be0bd6690d4fc46118b736ef08794dca5
Harsha patel <patelharsha371@gmail.com>
2026-02-20 23:07:20 +0530

    Fix DB init and column name

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 def init_db():
     conn = sqlite3.connect("tasks.db")
     cursor = conn.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            content TEXT NOT NULL,completed INTEGER DEFAULT 0
+            task TEXT NOT NULL,completed INTEGER DEFAULT 0
         )
     """)
     conn.commit()
     conn.close()
-
+init_db()
 @app.route("/")
 def index():
     conn = sqlite3.connect("tasks.db")
@@ -29,7 +30,7 @@ def add():
     task = request.form["content"]
     conn = sqlite3.connect("tasks.db")
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO tasks (content) VALUES (?)", (task,))
+    cursor.execute("INSERT INTO tasks (task) VALUES (?)", (task,))
     conn.commit()
     conn.close()
     return redirect("/")
